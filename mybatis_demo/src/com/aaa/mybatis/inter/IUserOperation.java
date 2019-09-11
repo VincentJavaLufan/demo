cffb616d458abaf5aedfca23fb6a416ce4d9f489
lufan <13027193761@163.com>
2019-09-12 06:12:14 +0800

    first commit

---
+++ mybatis_demo/src/com/aaa/mybatis/inter/IUserOperation.java
@@ -0,0 +1,15 @@
+package com.aaa.mybatis.inter;
+
+import com.aaa.mybatis.Emp;
+
+public interface IUserOperation {
+	 
+	public Emp selectEmpnoByEname(String ename);
+	
+	public void insert(Emp user);
+	
+	public void update(Emp user);
+	
+	public boolean deleteByEname(String ename);
+
+}
