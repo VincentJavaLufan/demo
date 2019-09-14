7db987c6d66ff114ecb9307927a17517685fbc0b
lufan <13027193761@163.com>
2019-09-15 00:30:50 +0800

    1111

---
+++ mybatis_demo/src/com/pwc/test/TestMain.java
@@ -0,0 +1,19 @@
+package com.pwc.test;
+
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+ 
+public class TestMain {
+	private static final Logger logger = Logger.getLogger(TestMain.class);
+	
+	private static final org.slf4j.Logger log = LoggerFactory.getLogger(TestMain.class);
+	
+	public static void main(String[] args) {
+//		PropertyConfigurator.configure("/log4j.properties");
+		
+		logger.info("DSFSDFSDF");
+		
+		log.info("SSS");
+		log.error("Email send error", "PPPPPPPP");
+	}
+}
