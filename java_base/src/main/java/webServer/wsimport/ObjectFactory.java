f20a36b888d42309aef4821a1d5dde7e77a1c8f4
Administrator <Administrator@LUFAN>
2019-11-10 23:41:38 +0800

    webservices

---
+++ java_base/src/main/java/webServer/wsimport/ObjectFactory.java
@@ -0,0 +1,71 @@
+
+package webServer.wsimport;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the webserver package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Ping_QNAME = new QName("http://webServer/", "ping");
+    private final static QName _PingResponse_QNAME = new QName("http://webServer/", "pingResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: webserver
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Ping }
+     * 
+     */
+    public Ping createPing() {
+        return new Ping();
+    }
+
+    /**
+     * Create an instance of {@link PingResponse }
+     * 
+     */
+    public PingResponse createPingResponse() {
+        return new PingResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Ping }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://webServer/", name = "ping")
+    public JAXBElement<Ping> createPing(Ping value) {
+        return new JAXBElement<Ping>(_Ping_QNAME, Ping.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link PingResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://webServer/", name = "pingResponse")
+    public JAXBElement<PingResponse> createPingResponse(PingResponse value) {
+        return new JAXBElement<PingResponse>(_PingResponse_QNAME, PingResponse.class, null, value);
+    }
+
+}
